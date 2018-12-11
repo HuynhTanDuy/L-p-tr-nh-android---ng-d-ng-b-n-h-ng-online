2e73e34511e889c5404dd6e3d71e95c37a3780db
NTD <nhokcute533@gmail.com>
2018-12-11 23:10:05 +0700

    14

--- app/src/main/java/util/Sever.java
+++ app/src/main/java/util/Sever.java
@@ -2,6 +2,6 @@ package util;
 
 public class Sever {
     public static String localhost = "192.168.1.17";
-    public static String producttypelink = "http://"+localhost+ ":8080/Sever/devicetype.php";
-    public static String newproduct = "http://"+localhost+":8080/Sever/newproduct.php";
+    public static String producttypelink = "http://"+localhost+ "/Sever/getdevicetype.php";
+    public static String newproduct = "http://"+localhost+":80/Sever/getnewproduct.php";
 }
